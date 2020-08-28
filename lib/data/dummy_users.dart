5d27fd58445e34608fcad11549466797ed62f1db
jsambarreto <jsambarreto@gmail.com>
2020-08-28 09:22:50 -0300

    Second commit

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: '1',
+      name: 'Paulo',
+      email: 'paulo@email.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2016/11/01/21/11/avatar-1789663_960_720.png'),
+  '2': const User(
+      id: '2',
+      name: 'Jorge',
+      email: 'jorge@email.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2018/05/19/22/03/man-3414477_960_720.png'),
+  '3': const User(
+      id: '3',
+      name: 'Karlos',
+      email: 'karlos@email.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2018/04/28/13/18/man-3357275_960_720.png'),
+  '4': const User(
+      id: '4',
+      name: 'Carmona',
+      email: 'carmona@email.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2017/01/31/21/23/avatar-2027367_960_720.png'),
+};
